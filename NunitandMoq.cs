4883b4a29659fa529bfb72dac65d881c7a2fbbea
Shxuryx-droid <shaurya962001@gmail.com>
2025-06-28 14:51:52 +0530

    Update NunitandMoq.cs

--- NunitandMoq.cs
+++ NunitandMoq.cs
@@ -1,1 +1,40 @@
+using NUnit.Framework;
+using CalculatorLib;
 
+namespace CalculatorLibTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator _calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calculator = new Calculator();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _calculator = null;
+        }
+
+        [Test]
+        [TestCase(2, 3, 5)]
+        [TestCase(0, 0, 0)]
+        [TestCase(-1, 1, 0)]
+        public void Add_ShouldReturnExpectedResult(int a, int b, int expected)
+        {
+            var result = _calculator.Add(a, b);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Ignore("Not implemented yet")]
+        public void Subtract_TestIgnored()
+        {
+            Assert.Pass();
+        }
+    }
+}
